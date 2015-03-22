9135aa7d3b4168d3dc7ee26ffbfa0f4c95920e43
bnbc <benjamin.beyssac@gmail.com>
2015-03-22 18:11:48 +0100

    Nettoyage

--- DependencyInjection/BnbcUploadExtension.php
+++ DependencyInjection/BnbcUploadExtension.php
@@ -22,10 +22,6 @@ class BnbcUploadExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
-
-        # On peut charger plusieurs types en même temps
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
